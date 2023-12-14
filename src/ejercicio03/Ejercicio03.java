8f2aa706a26d7bf65e43c0c80522b16b29610d41
Candi <Alcantarilla.cacan24@triana.salesianos.edu>
2023-12-14 08:01:52 +0100

    ejercicio03

--- src/ejercicio03/Ejercicio03.java
+++ src/ejercicio03/Ejercicio03.java
@@ -15,7 +15,7 @@ public class Ejercicio03 {
 	        
 	        System.out.println("Original Array : "+ Arrays.toString(my_array));     
 	        
-	        for(int i=my_array.length; i >= Index_position; i++){
+	        for(int i=0; i >= Index_position; i++){
 	            my_array[i] = my_array[i-1];
 	        }
 	        my_array[Index_position] = newValue;
